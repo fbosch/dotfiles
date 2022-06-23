a84da00ddb4119f98f8a72b4333c064049f62d11
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2022-06-23 20:51:39 +0200

    added peco functions

---
+++ .config/fish/functions/peco_select_history.fish
@@ -0,0 +1,16 @@
+# https://github.com/craftzdog/dotfiles-public/blob/master/.config/fish/functions/peco_select_history.fish
+function peco_select_history
+  if test (count $argv) = 0
+    set peco_flags --layout=bottom-up
+  else
+    set peco_flags --layout=bottom-up --query "$argv"
+  end
+
+  history|peco $peco_flags|read foo
+
+  if [ $foo ]
+    commandline $foo
+  else
+    commandline ''
+  end
+end
