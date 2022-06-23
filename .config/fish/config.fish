a9ba29743548ebe573b2844bcd35a2a299f67b70
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2022-06-23 20:24:32 +0200

    added peco shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,6 +13,27 @@ else
     end
 end
 
+# Switch directories using LF
+function lfcd --description "lf to switch directories"
+    set --local tmp "$(mktemp)"
+    eval command lf -last-dir-path="$tmp" "$argv"
+
+    if test -f "$tmp"
+        set --local dir "$(cat "$tmp")"
+        eval command rm -rf "$tmp" > /dev/null
+        if test -d "$dir" && test "$dir" != "$(pwd)"
+           cd "$dir"
+           commandline --function repaint
+        end
+    end
+end
+
+bind -M insert \cc kill-whole-line repaint
+bind \co 'lfcd' # Change directories when pressing CTRL+O
+# peco
+bind \cr 'peco_select_history' # Bind for peco select history to Ctrl+R
+bind \cf 'peco_change_directory' # Bind for peco change directory to Ctrl+F
+
 # Keybindings
 function fish_user_keybindings
   fish_vi_key_bindings
