07bd6b497ee24c494f280407d556f99d0a703191
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2024-02-09 14:38:57 +0100

    added wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,67 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.front_end = "WebGpu"
+config.max_fps = 120
+
+-- fonts
+config.font = wezterm.font_with_fallback({
+	{
+		family = "JetBrainsMono Nerd Font Mono",
+		harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+	},
+	"Noto Sans Runic",
+	{ family = "Symbols Nerd Font Mono", scale = 0.8 },
+	"Apple Color Emoji",
+})
+config.font_size = 16.0
+config.underline_thickness = "0.1cell"
+config.cursor_thickness = 1
+config.custom_block_glyphs = true
+
+-- colors
+config.color_scheme = "zenwritten_dark"
+config.tab_max_width = 64
+config.colors = {
+	tab_bar = {
+		background = "#191919",
+		active_tab = {
+			bg_color = "#525252",
+			fg_color = "#ffffff",
+			intensity = "Normal",
+		},
+		inactive_tab = {
+			bg_color = "#2e2e2e",
+			fg_color = "#9e9e9e",
+		},
+	},
+}
+
+-- window
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 3,
+	right = 3,
+	top = 3,
+	bottom = 3,
+}
+
+-- tabs
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+
+config.keys = {
+	{
+		key = "RightArrow",
+		mods = "CTRL",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "LeftArrow",
+		mods = "CTRL",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
+
+return config
