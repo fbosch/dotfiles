754dd0bdf2c2bf07c99bb421e4772d0b611e7c2b
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2024-02-12 17:32:18 +0100

    added brew install scripts and stow ignore file

---
+++ scripts/brew-install-script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+os_name=$(uname -s)
+case $os_name in
+    "Darwin")
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        ;;
+    "Linux")
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        test -d ~/.linuxbrew && eval "(~/.linuxbrew/bin/brew shellenv)"
+        test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+        ;;
+    *)
+        echo "Unknown or unsupported operating system"
+        ;;
+esac
+
+brew bundle install
+
+fish
+fish_add_path /opt/homebrew/bin
+echo "opt/homebrew/bin/fish" | sudo tee -a /etc/shells
+chsh -s opt/homebrew/bin/fish
