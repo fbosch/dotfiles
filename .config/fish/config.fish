5a539599d4eddd6f966ec2ff2a20c4558a268362
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2024-02-12 15:20:38 +0100

    stow for window wsl

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,12 @@
 . ~/.config/fish/aliases.fish
 . ~/.config/fish/profile.fish
 . ~/.config/fish/colors.fish
-. ~/.config/fish/nvm.fish
+
+if test uname = "Darwin"
+  . ~/.config/fish/nvm.fish
+end
 
 function fish_greeting
-  if [ "$KITTY_WINDOW_ID" = "1" ]
-  end
 end
 
 # Custom Function for a sudo !! replacement
@@ -19,13 +20,13 @@ end
 
 # Switch directories using LF
 function lfcd --description "lf to switch directories"
-    set --local tmp "$(mktemp)"
+    set --local tmp "(mktemp)"
     eval command lf -last-dir-path="$tmp" "$argv"
 
     if test -f "$tmp"
-        set --local dir "$(cat "$tmp")"
+        set --local dir "(cat "$tmp")"
         eval command rm -rf "$tmp" > /dev/null
-        if test -d "$dir" && test "$dir" != "$(pwd)"
+        if test -d "$dir" && test "$dir" != "(pwd)"
            cd "$dir"
            commandline --function repaint
         end
@@ -39,35 +40,37 @@ function fish_user_keybindings
   fish_vi_key_bindings
 end
 
-function set_nvm --on-event fish_prompt
-    # runs whenever the fish_prompt event occurs
-    # if the current directory hasn't changed, do nothing
-    string match -q $PWD $PREV_PWD; and return 1
+if test uname = "Darwin"
+  function set_nvm --on-event fish_prompt
+      # runs whenever the fish_prompt event occurs
+      # if the current directory hasn't changed, do nothing
+      string match -q $PWD $PREV_PWD; and return 1
 
-    # if the current directory is within the previous one where we found an nvmrc
-    # and there is no subsequent .nvmrc here, do nothing, we are in the same repo
-    string match -eq $PREV_PWD $PWD; and not test -e '.nvmrc'; and return 1
+      # if the current directory is within the previous one where we found an nvmrc
+      # and there is no subsequent .nvmrc here, do nothing, we are in the same repo
+      string match -eq $PREV_PWD $PWD; and not test -e '.nvmrc'; and return 1
 
-    # if we clear those checks, keep track of where we are
-    set -g PREV_PWD $PWD
+      # if we clear those checks, keep track of where we are
+      set -g PREV_PWD $PWD
 
-    if test -e '.nvmrc'
+      if test -e '.nvmrc'
 
-        # if we find .nvmrc, run nvm use
-        nvm use --silent
+          # if we find .nvmrc, run nvm use
+          nvm use --silent
 
-        # and remember that we used that node
-        set NVM_DIRTY true
+          # and remember that we used that node
+          set NVM_DIRTY true
 
-    else if not string match $NVM_DIRTY true
+      else if not string match $NVM_DIRTY true
 
-        # if we have set nvm and have stepped out of that repo
-        # go back to default node, if not already on it
-        not string match -eq (nvm current); and nvm use default --silent
+          # if we have set nvm and have stepped out of that repo
+          # go back to default node, if not already on it
+          not string match -eq (nvm current); and nvm use default --silent
 
-        # and clear the flag
-        set NVM_DIRTY
-    end
+          # and clear the flag
+          set NVM_DIRTY
+      end
+  end
 end
 
 zoxide init fish | source
