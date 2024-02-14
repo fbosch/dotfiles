dbce0dd6b96e7427a498c7651a051c26194c2035
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2024-02-14 14:20:11 +0100

    fix: adjust diagnostics signs

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -85,7 +85,7 @@ return {
 			-- vim.keymap.set("n", "<leader>f", vim.lsp.buf.formatting, bufopts)
 
 			-- floating diagnostics
-			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 			for type, icon in pairs(signs) do
 				local hl = "DiagnosticSign" .. type
 				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
