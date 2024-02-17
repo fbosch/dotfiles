42ad92369e99209c9a4d21168f06cd46b0e1e315
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2024-02-17 23:52:26 +0100

    adjust keymaps

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -1,60 +1,63 @@
 local map = vim.api.nvim_set_keymap
-local options = { noremap = true }
-local silent = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true }
 
 -- disable
-map("n", "<Space>", "<NOP>", silent)
-map("n", "q", "<NOP>", silent)
+map("n", "<Space>", "<NOP>", opts)
+map("n", "q", "<NOP>", opts)
 
 -- replace text
-map("n", "<leader>ra", ":Sad<CR>", silent)
+map("n", "<leader>ra", ":Sad<CR>", opts)
+
+-- increment and decrement
+map("n", "+", "<C-a>", opts)
+map("n", "-", "<C-x>", opts)
 
 -- yank
-map("n", "<C-a>", "ggVG<CR>", silent)
+map("n", "<C-a>", "ggVG<CR>", opts)
 
 -- save files
-map("n", "<leader>s", ":wa<CR>", silent)
-map("n", "<leader>S", ":wqa!<CR>", silent)
+map("n", "<leader>s", ":wa<CR>", opts)
+map("n", "<leader>S", ":wqa!<CR>", opts)
 
 -- easier indentation in visual mode
-map("v", "<", "<gv", options)
-map("v", ">", ">gv", options)
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
 
 -- autoswitch to newly created split
-map("n", "<C-W>v", ":vsplit<CR> <bar> :wincmd l<CR>", silent)
-map("n", "<C-W>s", ":split<CR> <bar> :wincmd j<CR>", silent)
+map("n", "<C-W>v", ":vsplit<CR> <bar> :wincmd l<CR>", opts)
+map("n", "<C-W>s", ":split<CR> <bar> :wincmd j<CR>", opts)
 
 -- remap split navigation to CTRL + hjkl
-map("n", "<S-h>", ":wincmd h<CR>", silent)
-map("n", "<S-j>", ":wincmd j<CR>", silent)
-map("n", "<S-k>", ":wincmd k<CR>", silent)
-map("n", "<S-l>", ":wincmd l<CR>", silent)
+map("n", "<S-h>", ":wincmd h<CR>", opts)
+map("n", "<S-j>", ":wincmd j<CR>", opts)
+map("n", "<S-k>", ":wincmd k<CR>", opts)
+map("n", "<S-l>", ":wincmd l<CR>", opts)
 
 -- remap split manipulation to SHIFT + CTRL + hjkl
-map("n", "<C-S-h>", ":wincmd H<CR>", silent)
-map("n", "<C-S-j>", ":wincmd J<CR>", silent)
+map("n", "<C-S-h>", ":wincmd H<CR>", opts)
+map("n", "<C-S-j>", ":wincmd J<CR>", opts)
 
-map("n", "<C-S-k>", ":wincmd K<CR>", silent)
-map("n", "<C-S-l>", ":wincmd L<CR>", silent)
+map("n", "<C-S-k>", ":wincmd K<CR>", opts)
+map("n", "<C-S-l>", ":wincmd L<CR>", opts)
 
 -- tab controls
--- map("n", "th", ":tabfirst<CR>", options)
--- map("n", "tj", ":tabprev<CR>", options)
--- map("n", "tk", ":tabnext<CR>", options)
--- map("n", "tl", ":tablast<CR>", options)
--- map("n", "tt", ":tabedit<Space>", options)
--- map("n", "tm", ":tabm<Space>", options)
--- map("n", "<C-t>n", ":tabnew<CR>", silent)
--- map("n", "<C-t>d", ":tabclose<CR>", silent)
+-- map("n", "th", ":tabfirst<CR>", opts)
+-- map("n", "tj", ":tabprev<CR>", opts)
+-- map("n", "tk", ":tabnext<CR>", opts)
+-- map("n", "tl", ":tablast<CR>", opts)
+-- map("n", "tt", ":tabedit<Space>", opts)
+-- map("n", "tm", ":tabm<Space>", opts)
+-- map("n", "<C-t>n", ":tabnew<CR>", opts)
+-- map("n", "<C-t>d", ":tabclose<CR>", opts)
 
 -- buffer controls
-map("n", "<leader>z", ":bp <bar> :bd #<CR>", silent) -- close buffer but keep split
-map("n", "<leader>0", ":b#<CR>", silent) --  previously active buffer
+map("n", "<leader>z", ":bp <bar> :bd #<CR>", opts) -- close buffer but keep split
+map("n", "<leader>0", ":b#<CR>", opts) --  previously active buffer
 
 -- move lines
-map("n", "<A-j>", ":m .+1<CR>==", silent)
-map("n", "<A-k>", ":m .-2<CR>==", silent)
-map("i", "<A-j>", "<Esc>:m .+1<CR>==gi", silent)
-map("i", "<A-k>", "<Esc>:m .-2<CR>==gi", silent)
-map("v", "<A-j>", ":m '>+1<CR>gv=gv", silent)
-map("v", "<A-k>", ":m '<-2<CR>gv=gv", silent)
+map("n", "<A-j>", ":m .+1<CR>==", opts)
+map("n", "<A-k>", ":m .-2<CR>==", opts)
+map("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+map("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
--- .config/nvim/lua/plugins/sad.lua
+++ .config/nvim/lua/plugins/sad.lua
@@ -2,4 +2,7 @@ return {
 	"ray-x/sad.nvim",
 	dependencies = { "ray-x/guihua.lua", run = "cd lua/fzy && make" },
 	cmd = { "Sad" },
+	config = function()
+		require("sad").setup()
+	end,
 }
