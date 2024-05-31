ec9029b2c3f353adfcd7c5967adfac32e3463cc4
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2024-05-31 11:04:22 +0200

    feat: added supermaven

--- .config/nvim/lua/plugins/editor/copilot.lua
+++ .config/nvim/lua/plugins/editor/copilot.lua
@@ -1,8 +1,22 @@
 return {
 	{
+		"supermaven-inc/supermaven-nvim",
+		event = "InsertEnter",
+		config = function()
+			require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<Tab>",
+					clear_suggestion = "<C-]>",
+					accept_word = "<C-j>",
+				},
+			})
+		end,
+	},
+	{
 		"zbirenbaum/copilot.lua",
 		cmd = "Copilot",
 		event = "InsertEnter",
+		enabled = false,
 		opts = {
 			filetypes = {
 				lua = true,
