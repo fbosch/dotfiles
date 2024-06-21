61b812e39ce475b5c49725abf246c675467be756
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2024-06-21 20:05:00 +0200

    feat: added home-nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,8 +1,40 @@
 { pkgs, ... }: {
   home.username = "fbb";
-  home.homeDirectory = "/Users/fbb";
+  home.homeDirectory = "/home/fbb";
   programs.home-manager.enable = true;
-  home.packages = {
-    pkgs.stow
-  }
+  home.stateVersion = "24.11";
+  home.packages = with pkgs; [
+    # system
+    stow
+    cmake
+    coreutils
+
+    # files
+    fzf
+    lf
+    bat
+    fd
+    eza
+    ripgrep
+    zoxide
+    cloc
+    grc
+
+    # terminal
+    starship
+    fish
+    wezterm
+    btop
+
+    # development 
+    git
+    lazygit
+    delta
+    neovim
+    bun
+    nodejs
+    fnm
+    jq
+    rustc
+  ];
 }
