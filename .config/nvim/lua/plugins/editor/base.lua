d4f9c94213145bfab4eab53fb261d2b5f0918216
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-03-03 15:16:50 +0100

    add nvim bqf

--- .config/nvim/lua/plugins/editor/base.lua
+++ .config/nvim/lua/plugins/editor/base.lua
@@ -1,5 +1,6 @@
 return {
 	{ "tpope/vim-repeat", event = "BufRead" },
+	{ "kevinhwang91/nvim-bqf", ft = "qf", event = "BufRead" },
 	{ "smjonas/inc-rename.nvim", event = "BufRead", opts = {} },
 	{ "windwp/nvim-autopairs", event = { "InsertEnter" }, opts = {} },
 	{ "monkoose/matchparen.nvim", event = { "InsertEnter" }, opts = {} },
