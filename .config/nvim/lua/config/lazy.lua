56ced010ff980b13dfcf7cdc411c956cac872ed9
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-05-25 12:36:53 +0200

    move lazy install script to init.lua

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,19 +1,3 @@
--- config/lazy.lua
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 vim.opt.loadplugins = true
 
 -- Define disabled built-in plugins
