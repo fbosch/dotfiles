157bb3577d3a8efa013a53ad26921ff76a87e068
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-05-31 12:34:33 +0200

    cleanup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,45 +1,7 @@
-if vim.env.PROF then
-	local snacks = vim.fn.stdpath("data") .. "/lazy/snacks.nvim"
-	vim.opt.rtp:append(snacks)
-	require("snacks.profiler").startup({
-		startup = {
-			event = "VimEnter",
-		},
-	})
-end
-
 vim.loader.enable()
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.spellfile_URL = "http://ftp.vim.org/vim/runtime/spell"
-
--- lite colorscheme
-vim.g.terminal_color_0 = "#191919"
-vim.g.terminal_color_1 = "#DE6E7C"
-vim.g.terminal_color_2 = "#819B69"
-vim.g.terminal_color_3 = "#B77E64"
-vim.g.terminal_color_4 = "#6099C0"
-vim.g.terminal_color_5 = "#B279A7"
-vim.g.terminal_color_6 = "#66A5AD"
-vim.g.terminal_color_7 = "#BBBBBB"
-vim.g.terminal_color_8 = "#3D3839"
-vim.g.terminal_color_9 = "#E8838F"
-vim.g.terminal_color_10 = "#8BAE68"
-vim.g.terminal_color_11 = "#D68C67"
-vim.g.terminal_color_12 = "#61ABDA"
-vim.g.terminal_color_13 = "#CF86C1"
-vim.g.terminal_color_14 = "#65B8C1"
-vim.g.terminal_color_15 = "#8E8E8E"
-
-require("utils").load_highlights({
-	Normal = { fg = "#BBBBBB", bg = "#191919" },
-	Comment = { fg = "#686868", bg = "#191919", italic = true },
-	Constant = { fg = "#8B8B8B", italic = true },
-	String = { fg = "#8B8B8B", italic = true },
-	LineNr = { fg = "#616161" },
-	Function = { fg = "#353535", italic = true },
-	Identifier = { fg = "#505050", italic = true },
-})
 
 require("config")
--- .config/nvim/lua/plugins/ui/visuals.lua
+++ .config/nvim/lua/plugins/ui/visuals.lua
@@ -61,24 +61,6 @@ return {
 		end,
 	},
 	{
-		"folke/snacks.nvim",
-		opts = function()
-			-- Toggle the profiler
-			Snacks.toggle.profiler():map("<leader>pp")
-			-- Toggle the profiler highlights
-			Snacks.toggle.profiler_highlights():map("<leader>ph")
-		end,
-		keys = {
-			{
-				"<leader>ps",
-				function()
-					Snacks.profiler.scratch()
-				end,
-				desc = "Profiler Scratch Buffer",
-			},
-		},
-	},
-	{
 		"m-demare/hlargs.nvim",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
