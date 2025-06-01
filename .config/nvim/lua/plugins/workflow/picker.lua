86604946867a0e0592ce0d49db9fa678fd69a144
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-06-01 17:31:02 +0200

    added nerdy

--- .config/nvim/lua/plugins/workflow/picker.lua
+++ .config/nvim/lua/plugins/workflow/picker.lua
@@ -42,4 +42,15 @@ return {
 			})
 		end,
 	},
+	{
+		"2kabhishek/nerdy.nvim",
+		dependencies = {
+			"folke/snacks.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		cmd = "Nerdy",
+		config = function()
+			require("telescope").load_extension("nerdy")
+		end,
+	},
 }
