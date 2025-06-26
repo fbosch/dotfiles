ec3e9eb42e215ba4abde3a7f7cf61b61d0b59a7d
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-06-26 12:43:59 +0200

    more vscode neovim configuration

--- .config/nvim/lua/plugins/ui/visuals.lua
+++ .config/nvim/lua/plugins/ui/visuals.lua
@@ -61,34 +61,6 @@ return {
 		end,
 	},
 	{
-		"m-demare/hlargs.nvim",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-		},
-		event = { "BufReadPre", "UIEnter", "LspAttach" },
-		config = function()
-			local hlargs = require("hlargs")
-			local colors = require("config.colors")
-			local colorpalette = {}
-			for _, color in ipairs(colors.highlight_args) do
-				table.insert(colorpalette, { fg = color })
-			end
-			hlargs.setup({
-				enabled = true,
-				use_colorpalette = true,
-				paint_catch_blocks = {
-					declarations = true,
-					usages = true,
-				},
-				extras = {
-					named_parameters = true,
-				},
-				colorpalette = colorpalette,
-				hl_priority = 300,
-			})
-		end,
-	},
-	{
 		"smjonas/live-command.nvim",
 		event = "CmdLineEnter",
 		config = function()
