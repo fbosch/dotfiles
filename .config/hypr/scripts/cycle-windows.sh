e7c13b6371ebceaf7c3217815c466521f3e75c2c
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-10-20 17:51:30 +0000

    cycle windows

--- .config/hypr/scripts/cycle-windows.sh
+++ .config/hypr/scripts/cycle-windows.sh
@@ -3,33 +3,19 @@
 direction="${1:-next}"
 
 current_address=$(hyprctl activewindow -j | jq -r '.address')
-all_windows=$(hyprctl clients -j | jq -r 'sort_by(.workspace.id) | .[] | select(.workspace.id != -1) | .address')
+# Use natural creation order to match Waybar taskbar order
+mapfile -t windows < <(hyprctl clients -j | jq -r '.[] | select(.workspace.id != -1) | .address')
 
-if [ -z "$all_windows" ]; then
-    exit 0
-fi
+[ ${#windows[@]} -eq 0 ] && exit 0
 
-window_array=()
-while IFS= read -r addr; do
-    window_array+=("$addr")
-done <<< "$all_windows"
-
-current_index=-1
-for i in "${!window_array[@]}"; do
-    if [ "${window_array[$i]}" = "$current_address" ]; then
-        current_index=$i
-        break
-    fi
+for i in "${!windows[@]}"; do
+    [ "${windows[$i]}" = "$current_address" ] && current_index=$i && break
 done
 
 if [ "$direction" = "prev" ]; then
-    next_index=$(( (current_index - 1 + ${#window_array[@]}) % ${#window_array[@]} ))
+    next_index=$(( (current_index - 1) % ${#windows[@]} ))
 else
-    next_index=$(( (current_index + 1) % ${#window_array[@]} ))
+    next_index=$(( (current_index + 1 + ${#windows[@]}) % ${#windows[@]} ))
 fi
 
-next_window="${window_array[$next_index]}"
-
-if [ -n "$next_window" ]; then
-    hyprctl dispatch focuswindow "address:$next_window"
-fi
+[ -n "${windows[$next_index]}" ] && hyprctl dispatch focuswindow "address:${windows[$next_index]}"
