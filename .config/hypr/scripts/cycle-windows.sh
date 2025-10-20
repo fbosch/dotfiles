b73775cb340c62cb83d11c44675be928d608a7b0
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-10-20 18:38:32 +0000

    chore: minor style tweaks

--- .config/hypr/scripts/cycle-windows.sh
+++ .config/hypr/scripts/cycle-windows.sh
@@ -3,8 +3,8 @@
 direction="${1:-next}"
 
 current_address=$(hyprctl activewindow -j | jq -r '.address')
-# Use natural creation order to match Waybar taskbar order
-mapfile -t windows < <(hyprctl clients -j | jq -r '.[] | select(.workspace.id != -1) | .address')
+# Sort by app_id (class) to match Waybar taskbar order
+mapfile -t windows < <(hyprctl clients -j | jq -r 'sort_by(.class) | .[] | select(.workspace.id != -1) | .address')
 
 [ ${#windows[@]} -eq 0 ] && exit 0
 
@@ -13,9 +13,9 @@ for i in "${!windows[@]}"; do
 done
 
 if [ "$direction" = "prev" ]; then
-    next_index=$(( (current_index - 1) % ${#windows[@]} ))
+    next_index=$(( (current_index + 1) % ${#windows[@]} ))
 else
-    next_index=$(( (current_index + 1 + ${#windows[@]}) % ${#windows[@]} ))
+    next_index=$(( (current_index - 1 + ${#windows[@]}) % ${#windows[@]} ))
 fi
 
 [ -n "${windows[$next_index]}" ] && hyprctl dispatch focuswindow "address:${windows[$next_index]}"
