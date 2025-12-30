f488e0c73e2978ab86aab221222775b97ccffb1b
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-12-30 17:46:03 +0100

    feat: add raw key code bindings for tab switching

--- .config/wezterm/lua/keys.lua
+++ .config/wezterm/lua/keys.lua
@@ -1,6 +1,53 @@
 local wezterm = require("wezterm")
 return function(config)
 	config.keys = {
+		-- Tab switching using raw key codes (works with any keyboard layout)
+		-- raw:10-18 are the physical number row keys 1-9
+		{
+			key = "raw:10",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivateTab(0),
+		},
+		{
+			key = "raw:11",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivateTab(1),
+		},
+		{
+			key = "raw:12",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivateTab(2),
+		},
+		{
+			key = "raw:13",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivateTab(3),
+		},
+		{
+			key = "raw:14",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivateTab(4),
+		},
+		{
+			key = "raw:15",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivateTab(5),
+		},
+		{
+			key = "raw:16",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivateTab(6),
+		},
+		{
+			key = "raw:17",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivateTab(7),
+		},
+		{
+			key = "raw:18",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivateTab(8),
+		},
 		{
 			key = "v",
 			mods = "CMD|SHIFT",
