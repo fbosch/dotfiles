eab3c875967f124c74b99ba83278a2170e8cf445
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-01-09 10:34:30 +0100

    feat: add Darwin support to nxrb function

--- .config/fish/functions/nxrb.fish
+++ .config/fish/functions/nxrb.fish
@@ -1,8 +1,17 @@
 function nxrb
-    if command -q nh
-        nh os switch ~/nixos
-    else
-        set -l host (hostname)
-        sudo nixos-rebuild switch --flake ~/nixos\#$host
+    switch (uname)
+        case Darwin
+            if command -q nh
+                nh darwin switch . -H rvn-mac
+            else
+                darwin-rebuild switch --flake ~/nixos\#rvn-mac
+            end
+        case Linux
+            if command -q nh
+                nh os switch ~/nixos
+            else
+                set -l host (hostname)
+                sudo nixos-rebuild switch --flake ~/nixos\#$host
+            end
     end
 end
