d87a30600cc6dec2f6e0dcaf0ebcbf1ab08d206c
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-01-24 21:13:20 +0100

    chore: revert

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -5,9 +5,6 @@ require("config.autocmd")
 require("config.abbr")
 require("config.lazy")
 
--- Enable OSC 52 clipboard for SSH/remote sessions
-require("utils.osc52").setup()
-
 if vim.g.vscode then
 	require("config.vscode")
 end
