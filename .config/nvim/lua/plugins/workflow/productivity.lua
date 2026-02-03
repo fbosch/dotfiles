ebb2bda6771d4cc38a55f1eeb89ea0cee009d9f8
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-02-03 20:39:45 +0100

    feat(nvim): add vim-wakatime plugin

--- .config/nvim/lua/plugins/workflow/productivity.lua
+++ .config/nvim/lua/plugins/workflow/productivity.lua
@@ -1,37 +1,43 @@
 return {
-	"bngarren/checkmate.nvim",
-	ft = "markdown",
-	config = function()
-		require("checkmate").setup({
-			todo_states = {
-				unchecked = {
-					marker = "󰄱 ",
-					order = 1,
+	{
+		"wakatime/vim-wakatime",
+		lazy = false,
+	},
+	{
+		"bngarren/checkmate.nvim",
+		ft = "markdown",
+		config = function()
+			require("checkmate").setup({
+				todo_states = {
+					unchecked = {
+						marker = "󰄱 ",
+						order = 1,
+					},
+					checked = {
+						marker = "󰄲 ",
+						order = 2,
+					},
 				},
-				checked = {
-					marker = "󰄲 ",
-					order = 2,
-				},
-			},
-		})
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = "markdown",
-			callback = function(args)
-				local filename = vim.fn.expand("%:t"):lower()
-				local targets = { "todo.md", ".todo.md" }
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "markdown",
+				callback = function(args)
+					local filename = vim.fn.expand("%:t"):lower()
+					local targets = { "todo.md", ".todo.md" }
 
-				local is_todo_file = false
-				for _, target in ipairs(targets) do
-					if filename == target then
-						is_todo_file = true
+					local is_todo_file = false
+					for _, target in ipairs(targets) do
+						if filename == target then
+							is_todo_file = true
+						end
 					end
-				end
 
-				if is_todo_file then
-					local map = require("utils").set_keymap
-					vim.opt_local.wrap = true
-				end
-			end,
-		})
-	end,
+					if is_todo_file then
+						local map = require("utils").set_keymap
+						vim.opt_local.wrap = true
+					end
+				end,
+			})
+		end,
+	},
 }
