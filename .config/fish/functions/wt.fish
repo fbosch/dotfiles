d071209d3a52146fc990c394b6dd89931145cd5a
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-03-06 23:22:05 +0100

    fix: wt

--- .config/fish/functions/wt.fish
+++ .config/fish/functions/wt.fish
@@ -1,8 +1,37 @@
 function wt
-    command wt config shell init fish | source
-    set -l wt_status $pipestatus[1]
-    set -l source_status $pipestatus[2]
-    test $wt_status -eq 0; or return $wt_status
-    test $source_status -eq 0; or return $source_status
-    wt $argv
+    set -l use_source false
+    set -l args
+
+    for arg in $argv
+        if test "$arg" = --source
+            set use_source true
+        else
+            set -a args $arg
+        end
+    end
+
+    test -n "$WORKTRUNK_BIN"; or set -l WORKTRUNK_BIN (type -P wt 2>/dev/null)
+    if test -z "$WORKTRUNK_BIN"
+        echo "wt: command not found" >&2
+        return 127
+    end
+    set -l directive_file (mktemp)
+
+    # --source: use cargo run (builds from source)
+    if test $use_source = true
+        env WORKTRUNK_DIRECTIVE_FILE=$directive_file cargo run --bin wt --quiet -- $args
+    else
+        env WORKTRUNK_DIRECTIVE_FILE=$directive_file $WORKTRUNK_BIN $args
+    end
+    set -l exit_code $status
+
+    if test -s "$directive_file"
+        source "$directive_file"
+        if test $exit_code -eq 0
+            set exit_code $status
+        end
+    end
+
+    rm -f "$directive_file"
+    return $exit_code
 end
