1586ef4cfd50b37c8ef606394c1713cc09d735fd
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-03-12 16:39:37 +0100

    fix(wezterm): drop pane from status update

--- .config/wezterm/lua/status.lua
+++ .config/wezterm/lua/status.lua
@@ -9,7 +9,7 @@ local color_white = { Color = "#bbbbbb" }
 -- Reusable status table structure
 local status = {}
 
-local function update_right_status(window, pane)
+local function update_right_status(window)
 	local date = wezterm.strftime("(%Y-%m-%d) %a %b %-d ")
 	local time = wezterm.strftime("%H:%M")
 	local week_number = os.date("%V")
@@ -29,44 +29,6 @@ local function update_right_status(window, pane)
 		{ Foreground = color_separator },
 		{ Text = "▕" },
 	}
-
-	local wday = os.date("*t").wday
-	if wday ~= 1 and wday ~= 7 then
-		local first_login = pane:get_user_vars().first_login
-		local calculated_hours =
-			require("lua.utils.time").calculate_hour_difference(first_login, wezterm.strftime("%H:%M:%S"))
-		local hours_worked = calculated_hours or 0
-		local icon
-		local color
-		
-		table.insert(status, { Text = " " })
-
-		if hours_worked > 0 and hours_worked < 10 then
-			if hours_worked > 8 then
-				icon = wezterm.nerdfonts.fa_hourglass_o
-				color = "#d79999"
-			elseif hours_worked >= 7 then
-				icon = wezterm.nerdfonts.fa_hourglass_o
-				color = "#819B69"
-			elseif hours_worked >= 5 then
-				icon = wezterm.nerdfonts.fa_hourglass_end
-				color = "#d2af0d"
-			elseif hours_worked >= 2 then
-				icon = wezterm.nerdfonts.fa_hourglass_half
-				color = "#B77E64"
-			else
-				icon = wezterm.nerdfonts.fa_hourglass_start
-				color = "#999999"
-			end
-			table.insert(status, { Foreground = { Color = color } })
-			local hours_string = string.format("%.1f", hours_worked):gsub("%.0", "")
-			table.insert(status, { Text = icon .. " " .. hours_string .. " " })
-		else
-			icon = wezterm.nerdfonts.fa_hourglass_start
-			table.insert(status, { Foreground = { Color = "#999999" } })
-			table.insert(status, { Text = icon .. " -.- " })
-		end
-	end
 
 	window:set_right_status(wezterm.format(status))
 end
