b5f54a1bd26b4e3719785d7032b261d8018b2b0c
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-03-17 10:56:09 +0100

    fix(fish): capture fzf selection directly

--- .config/fish/functions/fzfcd.fish
+++ .config/fish/functions/fzfcd.fish
@@ -1,13 +1,11 @@
 function fzfcd
-    set -l tmp (mktemp)
+    set -l selected_dir
     if test -f .git
         # if current directory is a .bare git repository, search for child directories contains .git folders only
-        fd -tf --max-depth=4 --color=never '.git$' -H | rev | cut -c 6- | rev | fzf --preview "lt {}" --preview-window "25%" > $tmp < /dev/tty
+        set selected_dir (fd -tf --max-depth=4 --color=never '.git$' -H | rev | cut -c 6- | rev | fzf --preview "lt {}" --preview-window "25%" < /dev/tty)
     else
-        fd -td --max-depth=4 --color=never --hidden | fzf --preview "lt {}" --preview-window "25%" > $tmp < /dev/tty
+        set selected_dir (fd -td --max-depth=4 --color=never --hidden | fzf --preview "lt {}" --preview-window "25%" < /dev/tty)
     end
-    set -l selected_dir (cat $tmp)
-    rm -f $tmp
     if test -n "$selected_dir"
         cd "$selected_dir"
         commandline --function repaint
