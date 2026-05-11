7d21022923027e179c65413b57226c5dc03d9326
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-05-11 14:54:27 +0200

    fix(fish): handle picker errors

--- .config/fish/functions/fzfcd.fish
+++ .config/fish/functions/fzfcd.fish
@@ -1,11 +1,18 @@
 function fzfcd
     set -l selected_dir
-    if test (git rev-parse --is-bare-repository 2>/dev/null) = true
+    set -l is_bare_repo (git rev-parse --is-bare-repository 2>/dev/null)
+    if test "$is_bare_repo" = true
         # In a bare repository, pick from child worktree directories containing a .git file.
         set selected_dir (fd -tf --max-depth=4 --color=never '.git$' -H -E .git -E .wt | rev | cut -c 6- | rev | fzf --preview "lt {}" --preview-window "25%" < /dev/tty)
     else
         set selected_dir (fd -td --max-depth=4 --color=never --hidden -E .git -E .wt -E node_modules -E .direnv -E .cache | fzf --preview "lt {}" --preview-window "25%" < /dev/tty)
     end
+
+    set -l picker_status $status
+    if test $picker_status -ne 0
+        return $picker_status
+    end
+
     if test -n "$selected_dir"
         cd "$selected_dir"
         commandline --function repaint
