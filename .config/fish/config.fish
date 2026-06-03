ea32e638e7622e3905d3390f1f3c93d2e94f89ce
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-06-03 11:01:32 +0200

    chore(config): remove pnpm path and fix typings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -193,10 +193,3 @@ if test -f ~/.inshellisense/key-bindings.fish
 end
 
 fish_add_path $HOME/.local/bin
-
-# pnpm
-set -gx PNPM_HOME "/home/fbb/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
