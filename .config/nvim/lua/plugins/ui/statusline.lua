7cc5be2e220528758e0fc361dbd545dc68b8aca4
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-06-09 13:12:28 +0200

    chore(config): tune nvim and toolbox config

--- .config/nvim/lua/plugins/ui/statusline.lua
+++ .config/nvim/lua/plugins/ui/statusline.lua
@@ -69,16 +69,10 @@ return {
 		local lualine_c = {
 			{
 				function()
-					-- Lazy-load git-blame only when needed
-					local ok, git_blame = pcall(require, "gitblame")
-					if ok then
-						return git_blame.get_current_blame_text()
-					end
-					return ""
+					return vim.b.gitsigns_blame_line or ""
 				end,
 				cond = function()
-					local ok, git_blame = pcall(require, "gitblame")
-					return git.is_git_repo() and ok and git_blame.is_blame_text_available()
+					return git.is_git_repo() and is_valid_status(vim.b.gitsigns_blame_line)
 				end,
 			},
 		}
