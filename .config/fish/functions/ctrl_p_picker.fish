5be37ef965df6fed2492107c84107402d6948ee0
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-06-18 14:05:58 +0200

    chore(config): update picker and models

--- .config/fish/functions/ctrl_p_picker.fish
+++ .config/fish/functions/ctrl_p_picker.fish
@@ -1,23 +1,15 @@
 function ctrl_p_picker --description 'Use git worktree picker when available'
-    set -l picker_status 0
+    set -l picker fzfcd
 
-    if not git rev-parse --git-dir >/dev/null 2>&1
-        fzfcd
-        set picker_status $status
-        if status --is-interactive
-            commandline --function repaint repaint-mode
+    if git rev-parse --git-dir >/dev/null 2>&1
+        set -l worktree_count (git worktree list --porcelain 2>/dev/null | string match -r '^worktree ' | count)
+        if test $worktree_count -gt 1
+            set picker wtfzf
         end
-        return $picker_status
     end
 
-    set -l worktree_count (git worktree list --porcelain 2>/dev/null | string match -r '^worktree ' | count)
-    if test $worktree_count -gt 1
-        wtfzf
-        set picker_status $status
-    else
-        fzfcd
-        set picker_status $status
-    end
+    $picker
+    set -l picker_status $status
 
     if status --is-interactive
         commandline --function repaint repaint-mode
