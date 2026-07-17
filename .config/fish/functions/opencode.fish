0fac9f96c36fb3874a64366088c25cae1dc50af1
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-07-17 10:39:53 +0200

    fix(fish): stop unsetting api key

--- .config/fish/functions/opencode.fish
+++ .config/fish/functions/opencode.fish
@@ -2,18 +2,18 @@ function opencode --wraps=opencode --description 'Run OpenCode, preferring Nix p
     set -l opencode_path (__opencode_command_path)
     if test -n "$opencode_path"
         if type -q mullvad-exclude
-            command env -u OPENAI_API_KEY mullvad-exclude $opencode_path $argv
+            command mullvad-exclude $opencode_path $argv
             return $status
         end
 
-        command env -u OPENAI_API_KEY $opencode_path $argv
+        command $opencode_path $argv
         return $status
     end
 
     if type -q mullvad-exclude
-        command env -u OPENAI_API_KEY mullvad-exclude opencode $argv
+        command mullvad-exclude opencode $argv
         return $status
     end
 
-    command env -u OPENAI_API_KEY opencode $argv
+    command opencode $argv
 end
