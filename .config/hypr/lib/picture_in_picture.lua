5c613ddcd0910fe3ef222733dfa558b33b3a37cb
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-07-31 11:36:24 +0200

    fix(hypr): raise pip overlap gap

--- .config/hypr/lib/picture_in_picture.lua
+++ .config/hypr/lib/picture_in_picture.lua
@@ -3,13 +3,9 @@ local M = {
 	title = "Picture-in-Picture",
 	right_margin = 15,
 	bottom_margin = 15,
-	overlap_gap = 10,
+	overlap_gap = 15,
 }
 
-M.normal_move = string.format(
-	"(monitor_w-window_w-%d) (monitor_h-window_h-%d)",
-	M.right_margin,
-	M.bottom_margin
-)
+M.normal_move = string.format("(monitor_w-window_w-%d) (monitor_h-window_h-%d)", M.right_margin, M.bottom_margin)
 
 return M
