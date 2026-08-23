30f271a98b61eefaa236e0579a4169e9a080bec6
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-08-23 12:34:43 +0200

    chore: update neovim

--- .config/hypr/base.lua
+++ .config/hypr/base.lua
@@ -1,24 +1,25 @@
 -- Base compositor settings ported from hyprland.conf.
 
 hl.config({
-  general = {
-    snap = {
-      enabled = true,
-      window_gap = 25,
-      monitor_gap = 10,
-    },
-  },
-  debug = {
-    disable_logs = true,
-    enable_stdout_logs = false,
-  },
-  xwayland = {
-    force_zero_scaling = true,
-  },
-  opengl = {
-    nvidia_anti_flicker = true,
-  },
-  render = {
-    direct_scanout = false,
-  },
+	general = {
+		snap = {
+			enabled = true,
+			window_gap = 25,
+			monitor_gap = 10,
+		},
+	},
+	debug = {
+		disable_logs = true,
+		enable_stdout_logs = false,
+	},
+	xwayland = {
+		force_zero_scaling = true,
+	},
+	opengl = {
+		nvidia_anti_flicker = true,
+	},
+	render = {
+		direct_scanout = false,
+		expand_undersized_textures = false,
+	},
 })
