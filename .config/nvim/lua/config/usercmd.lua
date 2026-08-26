c8e64dda96eef73e380f42688e3bc6428612770d
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-08-26 13:21:54 +0200

    feat(nvim): add keymap reload command

--- .config/nvim/lua/config/usercmd.lua
+++ .config/nvim/lua/config/usercmd.lua
@@ -1,5 +1,12 @@
 local utils = require("utils")
 local usrcmd = utils.set_usrcmd
+local keymap_modules = {
+	"config.keymaps.core",
+	"config.keymaps.navigation",
+	"config.keymaps.yank",
+	"config.keymaps.editing",
+	"config.keymaps.plugins",
+}
 
 -- fix for the Z command
 usrcmd("Z", "wa | qa")
@@ -38,6 +45,15 @@ usrcmd("RefreshUsage", function()
 	end
 	vim.notify("Refreshing usage stats...", vim.log.levels.INFO)
 end, { desc = "Clear cache and refetch usage stats for all providers" })
+
+usrcmd("ReloadConfig", function()
+	-- Only reload keymaps: other config modules register commands and autocmds that are not reload-safe.
+	for _, module_name in ipairs(keymap_modules) do
+		package.loaded[module_name] = nil
+		require(module_name)
+	end
+	vim.notify("Reloaded Neovim keymaps", vim.log.levels.INFO)
+end, "Reload Neovim keymaps")
 
 usrcmd("PackUpdate", function(args)
 	vim.pack.update(#args.fargs > 0 and args.fargs or nil)
